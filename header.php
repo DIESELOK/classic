6b1bf2e8ee4baf404e6e35f0cef1de1e639f74fe
Aleksey Dmitrenko <DIESELOKJ@gmail.com>
2016-03-04 20:36:21 +0200

    footer, header

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <title><?php bloginfo('name'); ?><?php wp_title('|'); ?></title>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(  ); ?>>
+
+
